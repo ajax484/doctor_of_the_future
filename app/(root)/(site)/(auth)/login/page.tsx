f883a81a3f210988a2b46b2d01e342f8d2549b4e
Ossy <93939723+codingossy@users.noreply.github.com>
2023-09-03 19:01:12 +0100

    Update login page.tsx

--- app/(root)/(site)/(auth)/login/page.tsx
+++ app/(root)/(site)/(auth)/login/page.tsx
@@ -5,7 +5,6 @@ export default function login() {
   return (
     <div className=" w-full px-4 md:px-8 lg:px-16 space-y-4 pt-6">
       <h1 className="text-2xl font-black text-center">Sign In/Up</h1>
-
       <div className="flex items-center gap-10 my-5 justify-center">
         <div className=" w-full border p-4 md:p-12">
           <AuthForm />
@@ -14,13 +13,9 @@ export default function login() {
         {/* image */}
         <div className=" w-full hidden md:block">
           <Image alt="image" src={"https://images.pexels.com/photos/979310/pexels-photo-979310.jpeg?auto=compress&cs=tinysrgb&w=800"} width={500} height={500} className=" w-full h-full" />
-
-      <div className="flex justify-center">
-        <div className="md:w-1/2">
-        <AuthForm />
-
         </div>
       </div>
     </div>
   );
 }
+
