c8957372274f1b1e72be36a6a017a92b8ac7ff8f
Yusuf Bilal <ubahyusuf484@gmail.com>
2023-09-03 16:09:46 +0100

    login page UI cleanup

--- app/(root)/(site)/(auth)/login/page.tsx
+++ app/(root)/(site)/(auth)/login/page.tsx
@@ -5,7 +5,7 @@ export default function login() {
     <div className="px-4 md:px-8 lg:px-16 space-y-4 pt-6">
       <h1 className="text-2xl font-black text-center">Sign In/Up</h1>
       <div className="flex justify-center">
-        <div className="w-1/2">
+        <div className="md:w-1/2">
         <AuthForm />
         </div>
       </div>
