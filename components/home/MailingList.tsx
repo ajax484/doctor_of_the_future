f7e6e2dde43dd62ad4b7208d8b380bb96611c272
Yusuf Bilal <ubahyusuf484@gmail.com>
2023-09-07 04:48:27 +0100

    more button fixing

--- components/home/MailingList.tsx
+++ components/home/MailingList.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Button from '../ui/button'
+import Button from "@/components/ui/customButton";
 import Input from '../ui/inputbase'
 
 export default function MailingList() {
