efdcbd6a73d8c11490fa53c900bb25034fff0cfa
ossy vincent <codingossy@gmail.com>
2023-09-07 16:55:16 +0100

    added terms, conditions and privacy policies

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,10 +10,20 @@ module.exports = {
   theme: {
     container: {
       center: true,
-      padding: "2rem",
-      screens: {
-        "2xl": "1400px",
+      padding: {
+        DEFAULT: "1rem",
+        sm: "1.5rem",
+        lg: "3rem",
+        xl: "4rem",
+        "2xl": "6rem",
       },
+    },
+    screens: {
+      xs: "375px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
     },
     extend: {
       colors: {
