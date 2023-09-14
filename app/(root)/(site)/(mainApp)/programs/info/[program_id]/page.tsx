32afa41cae41d2ba9ad92a44a6bb3f36494e27c5
Yusuf Bilal <ubahyusuf484@gmail.com>
2023-09-14 05:39:15 +0100

    WIP: programs section

--- app/(root)/(site)/(mainApp)/programs/info/[program_id]/page.tsx
+++ app/(root)/(site)/(mainApp)/programs/info/[program_id]/page.tsx
@@ -22,26 +22,10 @@ export default async function programInfo({
   }
 
   return (
-    <section className="md:px-4 lg:px-8">
-      <div className="p-4 md:p-8 lg:p-16 bg-limeGreen/50 space-y-6">
-        <h2 className="text-2xl font-bold text-slate-800">
-          {program.name} | <span className="uppercase">{program.gender}</span>
-        </h2>
-        <div className="flex gap-8">
-          <div className="flex flex-col justify-between">
-            <span className="block text-slate-800 text-lg">
-              {program.duration}
-            </span>
-            <span className="block text-slate-600">Days</span>
-          </div>
-          <div className="h-16 w-[2px] bg-gray-400" />
-          <div className="flex flex-col justify-between">
-            <span className="block text-slate-800 text-lg">
-              {program.steps}
-            </span>
-            <span className="block text-slate-600">Steps</span>
-          </div>
-        </div>
+    <section className="md:px-4 lg:px-12">
+      <div className="p-4 md:p-8 lg:p-16 bg-limeGreen/20 space-y-6">
+        <h2 className="text-2xl font-bold text-slate-800">{program.name}</h2>
+        <span className="block text-slate-800 text-lg">{program.duration}</span>
         <div className="space-y-2 text-slate-700">
           <p>Get a certificate by completing the program.</p>
           <p>
@@ -50,7 +34,7 @@ export default async function programInfo({
           </p>
         </div>
       </div>
-      <div className="w-full relative h-40 md:h-80 lg:h-96 mb-6 md:mb-12 lg:mb-24">
+      <div className="w-full relative h-40 md:h-80 lg:h-96 mb-2 md:mb-6 lg:mb-12">
         <Image
           src={program.image}
           blurDataURL={`data:image/svg+xml;base64,${toBase64(
@@ -64,14 +48,14 @@ export default async function programInfo({
       </div>
 
       <div className="flex flex-col md:flex-row gap-2">
-        <div className="flex-[70%] px-4 md:px-8 lg:px-16 space-y-6 md:space-y-8 lg:space-y-12">
+        <div className="flex-[70%] px-4 md:px-8 lg:px-16 space-y-6 md:space-y-8 lg:space-y-12 pt-3 md:pt-6 lg:pt-12">
           <div className="space-y-2 md:space-y-4 lg:space-y-8">
             <h3 className="text-2xl">About</h3>
             <p className="text-slate-600">{program.description}</p>
           </div>
           <Button label="Join" intent="primary" />
         </div>
-        <div className="bg-limeGreen/50 w-full md:min-h-[400px] flex-[30%]" />
+        <div className="bg-limeGreen/30 w-full md:min-h-[600px] flex-[30%] hidden md:block" />
       </div>
     </section>
   );
