4dafdcdfedd1794698ce1f6be0eba2f19f5b03c0
ossy vincent <codingossy@gmail.com>
2023-09-14 01:05:17 +0100

    feat: added programs and programs slug

--- app/(root)/(site)/(mainApp)/programs/info/[program_id]/page.tsx
+++ app/(root)/(site)/(mainApp)/programs/info/[program_id]/page.tsx
@@ -3,6 +3,7 @@ import { DUMMY_PROGRAMS } from "../../page";
 import Image from "next/image";
 import Button from "@/components/ui/customButton";
 import { shimmer, toBase64 } from "@/utils/shimmerimage";
+import ProgramsDetails from "@/components/ui/programs/ProgramsDetails";
 
 async function getProgram(program_id: number) {
   return DUMMY_PROGRAMS.find((program) => program.id == program_id);
@@ -24,27 +25,21 @@ export default async function programInfo({
   return (
     <section className="md:px-4 lg:px-8">
       <div className="p-4 md:p-8 lg:p-16 bg-limeGreen/50 space-y-6">
-        <h2 className="text-2xl font-bold text-slate-800">
+        <h2 className=" text-2xl lg:text-4xl font-bold text-slate-800">
           {program.name} | <span className="uppercase">{program.gender}</span>
         </h2>
-        <div className="flex gap-8">
-          <div className="flex flex-col justify-between">
-            <span className="block text-slate-800 text-lg">
+        <div className="flex items-start gap-8">
+          <div className="">
+            <span className="block capitalize font-semibold text-xl text-slate-800">
               {program.duration}
             </span>
-            <span className="block text-slate-600">Days</span>
-          </div>
-          <div className="h-16 w-[2px] bg-gray-400" />
-          <div className="flex flex-col justify-between">
-            <span className="block text-slate-800 text-lg">
-              {program.steps}
-            </span>
-            <span className="block text-slate-600">Steps</span>
           </div>
         </div>
-        <div className="space-y-2 text-slate-700">
-          <p>Get a certificate by completing the program.</p>
-          <p>
+        <div className="space-y-2 text-slate-800">
+          <p className=" text-slate-800">
+            Get a certificate by completing the program.
+          </p>
+          <p className=" text-slate-800">
             Everyone who has completed all the steps will get a badge when the
             program ends.
           </p>
@@ -52,7 +47,9 @@ export default async function programInfo({
       </div>
       <div className="w-full relative h-40 md:h-80 lg:h-96 mb-6 md:mb-12 lg:mb-24">
         <Image
-          src={program.image}
+          src={
+            "https://images.pexels.com/photos/791763/pexels-photo-791763.jpeg?auto=compress&cs=tinysrgb&w=800"
+          }
           blurDataURL={`data:image/svg+xml;base64,${toBase64(
             shimmer(200, 200)
           )}`}
@@ -63,15 +60,23 @@ export default async function programInfo({
         />
       </div>
 
-      <div className="flex flex-col md:flex-row gap-2">
-        <div className="flex-[70%] px-4 md:px-8 lg:px-16 space-y-6 md:space-y-8 lg:space-y-12">
-          <div className="space-y-2 md:space-y-4 lg:space-y-8">
-            <h3 className="text-2xl">About</h3>
-            <p className="text-slate-600">{program.description}</p>
-          </div>
-          <Button label="Join" intent="primary" />
+      <div className="flex flex-col w-full md:flex-row gap-x-5 gap-y-8">
+        <ProgramsDetails program={program} />
+
+        <div className="bg-limeGreen/50 w-full md:min-h-[400px]">
+          <Image
+            src={
+              "https://images.pexels.com/photos/28080/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=800"
+            }
+            blurDataURL={`data:image/svg+xml;base64,${toBase64(
+              shimmer(200, 200)
+            )}`}
+            alt="image"
+            className="w-full h-full object-cover"
+            width={500}
+            height={500}
+          />
         </div>
-        <div className="bg-limeGreen/50 w-full md:min-h-[400px] flex-[30%]" />
       </div>
     </section>
   );
