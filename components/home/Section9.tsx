70969ad5266848decfb5c1c2a584fe012ca2c5f4
ossy vincent <codingossy@gmail.com>
2023-09-24 11:40:48 +0100

    fix: changes to blog page

--- components/home/Section9.tsx
+++ components/home/Section9.tsx
@@ -2,19 +2,19 @@ import React from "react";
 
 const Section9 = () => {
   return (
-    <div className="bg-gradient-to-r from-pink-500 via-pink-400 to-pink-500 w-full h-96">
+    <div className=" bg-orange w-full h-96">
       <div className="text-center flex flex-col gap-y-5 py-16">
         <h1 className="text-2xl xs:text-4xl font-bold">
           Remember, your{" "}
-          <span className="font-black uppercase text-white text-4xl xs:text-5xl md:text-7xl">
+          <span className="font-black italic uppercase text-white text-2xl xs:text-3xl md:text-4xl">
             health
           </span>{" "}
           is your first{" "}
-          <span className="uppercase font-black text-white text-4xl xs:text-5xl lg:text-7xl">
+          <span className="uppercase italic font-black text-white text-2xl xs:text-3xl lg:text-4xl">
             wealth
           </span>
         </h1>
-        <h1 className="text-4xl xs:text-5xl lg:text-7xl text-white font-black underline">
+        <h1 className="text-4xl xs:text-5xl italic lg:text-7xl text-white font-black underline">
           #StayHealthy
         </h1>
       </div>
